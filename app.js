28b374dfdbc24ff7e3370293f92eedd0f8d8b8d4
kalmipa21 <aripsapu@gmail.com>
2023-04-07 16:44:36 +0700

    Push Fixed

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const users = require("./routers/user.js");
 const app = express();
 const PORT = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(users);
 app.use("/", (req, res) => {
